e68a92ad7d1e63f2306399b6dd5bd635a93ecf9f
Habraken,Noud N.H.H <n.habraken@student.fontys.nl>
2019-06-17 11:34:58 +0200

    Update Actor.js

--- src/main/webapp/WEB-INF/view/react/src/components/Actor.js
+++ src/main/webapp/WEB-INF/view/react/src/components/Actor.js
@@ -8,7 +8,7 @@ class Actor extends Component {
                 <Link to={"/actor?id=" + this.props.id} style={{color: "#212529", textDecoration: "none"}}>
                     <div className={"row ActorRow"}>
                         <div className={"col-md-2"} style={{"max-height": "100%"}}>
-                            <img src={this.props.imageUrl}/>
+                            <img src={this.props.imageUrl} height={"100%"}/>
                         </div>
                         <div className={"col-md-10"}>
                             <p className={"mb-0 ml-1"}>{this.props.name}</p>
